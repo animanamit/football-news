b474d0dc0ddcef36c69d242a64ea2e849ed937f3
Animan Amit <animanamit@wustl.edu>
2022-06-27 15:24:58 +0800

    removed any from fixture table

--- src/components/fixture-table.tsx
+++ src/components/fixture-table.tsx
@@ -1,10 +1,17 @@
 import axios from "axios";
 import { useQuery } from "react-query";
+import { AnyZodObject } from "zod";
 import { useStore } from "../pages";
 
-async function fetchFixtures({ queryKey }: any) {
-  const [_key, obj] = queryKey;
+type Fixture = {
+  fixture: object;
+  goals: object;
+  league: object;
+  teams: object;
+  score: object;
+};
 
+async function fetchFixtures(season: number, teamId: number) {
   const dataObj = await axios
     .get("https://api-football-v1.p.rapidapi.com/v3/fixtures", {
       method: "GET",
@@ -12,7 +19,7 @@ async function fetchFixtures({ queryKey }: any) {
         "x-rapidapi-host": process.env.NEXT_PUBLIC_RAPID_API_HOST as string,
         "x-rapidapi-key": process.env.NEXT_PUBLIC_RAPID_API_KEY as string,
       },
-      params: { team: obj.teamId, season: obj.season },
+      params: { team: teamId, season: season },
     })
     .then((res) => {
       return res.data.response;
@@ -22,16 +29,17 @@ async function fetchFixtures({ queryKey }: any) {
   let data = Object.keys(dataObj).map((key) => {
     return dataObj[key];
   });
+
   return data;
 }
 
-const FixtureTable: any = () => {
+const FixtureTable = () => {
   const season = useStore((state: { season: number }) => state.season);
   const teamId = useStore((state: { teamId: number }) => state.teamId);
 
-  const { data, error, isLoading } = useQuery(
-    ["fixtures", { season: season, teamId: teamId }],
-    fetchFixtures
+  const { data, error, isLoading } = useQuery<Fixture[], Error>(
+    ["fixtures", { season, teamId }],
+    () => fetchFixtures(season, teamId)
   );
 
   if (isLoading)
@@ -39,15 +47,6 @@ const FixtureTable: any = () => {
       <div className="h-fit flex flex-col rounded-xl py-2 px-4 w-1/3 mt-2 bg-[rgba(23,12,61,0.4)] overflow-hidden  shadow-[rgb(255 255 255 / 20%) 0px 0px 0px 0.5px inset] backdrop-blur-md">
         <h4 className="py-2 text-xl font-semibold leading-10 text-center uppercase tracking-loose text-slate-200">
           Loading...
-        </h4>
-      </div>
-    );
-
-  if (error)
-    return (
-      <div className="h-fit flex flex-col rounded-xl py-2 px-4 w-1/3 mt-2 bg-[rgba(23,12,61,0.4)] overflow-hidden  shadow-[rgb(255 255 255 / 20%) 0px 0px 0px 0.5px inset] backdrop-blur-md">
-        <h4 className="py-2 text-xl font-semibold leading-10 text-center uppercase tracking-loose text-slate-200">
-          Error! Please reload this page!
         </h4>
       </div>
     );
@@ -89,6 +88,14 @@ const FixtureTable: any = () => {
       </div>
     );
   }
+
+  return (
+    <div className="h-fit flex flex-col rounded-xl py-2 px-4 w-1/3 mt-2 bg-[rgba(23,12,61,0.4)] overflow-hidden  shadow-[rgb(255 255 255 / 20%) 0px 0px 0px 0.5px inset] backdrop-blur-md">
+      <h4 className="py-2 text-xl font-semibold leading-10 text-center uppercase tracking-loose text-slate-200">
+        Error! Please reload this page!
+      </h4>
+    </div>
+  );
 };
 
 export default FixtureTable;
