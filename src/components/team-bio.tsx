b57535b5942769896c0bfd83cc3ff12bf7464a82
Animan Amit <animanamit@wustl.edu>
2022-06-27 16:25:44 +0800

    removed any from team-bio

--- src/components/team-bio.tsx
+++ src/components/team-bio.tsx
@@ -1,39 +1,44 @@
-import axios from "axios";
 import { useQuery } from "react-query";
 import { useStore } from "../pages";
 
-async function fetchBio({ queryKey }: any): Promise<any> {
-  const [_key, obj] = queryKey;
-  const data = await axios
-    .get("https://api-football-v1.p.rapidapi.com/v3/teams", {
-      method: "GET",
-      headers: {
-        "x-rapidapi-host": process.env.NEXT_PUBLIC_RAPID_API_HOST as string,
-        "x-rapidapi-key": process.env.NEXT_PUBLIC_RAPID_API_KEY as string,
-      },
-      params: { id: obj.teamId },
-    })
-    .then((res) => {
-      return res.data.response[0];
-    });
+async function fetchBio(teamId: number) {
+  let url = new URL("https://api-football-v1.p.rapidapi.com/v3/teams");
+  url.searchParams.append("id", teamId.toString());
 
-  console.log(data);
+  const data = await fetch(url.toString(), {
+    method: "GET",
+    headers: {
+      "x-rapidapi-host": process.env.NEXT_PUBLIC_RAPID_API_HOST as string,
+      "x-rapidapi-key": process.env.NEXT_PUBLIC_RAPID_API_KEY as string,
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => data.response[0]);
 
   return data;
 }
 
+type TeamData = {
+  team: {
+    name: string;
+  };
+  venue: {
+    image: string;
+  };
+};
+
 const TeamBio = () => {
   const teamId = useStore((state: { teamId: number }) => state.teamId);
 
-  const { data, error, isLoading } = useQuery(
-    ["bio", { teamId: teamId }],
-    fetchBio
+  const { data, error, isLoading } = useQuery<TeamData, Error>(
+    ["bio", teamId],
+    () => fetchBio(teamId)
   );
 
   if (isLoading)
     return (
-      <div className="rounded-xl w-4/5 h-52 relative">
-        <h4 className="text-white absolute font-semibold tracking-wide uppercase right-4 bottom-7 text-7xl opacity-60">
+      <div className="relative w-4/5 rounded-xl h-52">
+        <h4 className="absolute font-semibold tracking-wide text-white uppercase right-4 bottom-7 text-7xl opacity-60">
           Loading...
         </h4>
       </div>
@@ -41,27 +46,30 @@ const TeamBio = () => {
 
   if (error)
     return (
-      <div className="rounded-xl w-4/5 h-52 relative">
-        <h4 className="text-white absolute font-semibold tracking-wide uppercase right-4 bottom-7 text-7xl opacity-60">
+      <div className="relative w-4/5 rounded-xl h-52">
+        <h4 className="absolute font-semibold tracking-wide text-white uppercase right-4 bottom-7 text-7xl opacity-60">
           Error! Please reload this page!
         </h4>
       </div>
     );
+  if (data) {
+    return (
+      <div className="relative w-4/5 rounded-xl h-52">
+        <div className="w-full h-full overflow-hidden">
+          <img
+            src={data.venue.image}
+            alt={`${data.team.name}'s home ground`}
+            className="object-cover w-full h-full overflow-hidden rounded-xl opacity-80"
+          />
+        </div>
+        <div className="absolute text-5xl font-semibold tracking-wide text-white uppercase right-4 bottom-7 opacity-60">
+          <h5>{data.team.name}</h5>
+        </div>
+      </div>
+    );
+  }
 
-  return (
-    <div className="rounded-xl w-4/5 h-52 relative">
-      <div className="h-full w-full overflow-hidden">
-        <img
-          src={data.venue.image}
-          alt={`${data.team.name}'s home ground`}
-          className="w-full rounded-xl h-full object-cover overflow-hidden opacity-80"
-        />
-      </div>
-      <div className="text-white absolute font-semibold tracking-wide uppercase right-4 bottom-7 text-5xl opacity-60">
-        <h5>{data.team.name}</h5>
-      </div>
-    </div>
-  );
+  return <></>;
 };
 
 export default TeamBio;
